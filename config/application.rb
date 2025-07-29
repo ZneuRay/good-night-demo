67659ada04bf6432d942d6664278b88d61115cbc
Ray <ray9015@gmail.com>
2025-07-29 13:22:10 +0800

    chore: update time zone

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module GoodNightDemo
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.time_zone = 'Asia/Taipei'
   end
 end
