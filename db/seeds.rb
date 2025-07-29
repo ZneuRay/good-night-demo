e474be64e8ddf5d6edc87aef57147ba68772ea28
Ray <ray9015@gmail.com>
2025-07-29 12:24:18 +0800

    chore: add user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(name: "Alice Johnson")
+User.create(name: "Bob Smith")
+User.create(name: "John Doe")
+User.create(name: "Admin User")
