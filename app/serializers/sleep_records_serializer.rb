ad78ab079b9951103c09a0c7a74eeaac27c55f0a
Ray <ray9015@gmail.com>
2025-07-30 22:29:23 +0800

    fix(sleep_records_serializer): handle nil clock_out_time in serializer

--- app/serializers/sleep_records_serializer.rb
+++ app/serializers/sleep_records_serializer.rb
@@ -6,7 +6,7 @@ class SleepRecordsSerializer
     record.clock_in_time.strftime("%Y-%m-%d %H:%M:%S")
   end
   attribute :clock_out_time do |record|
-    record.clock_out_time.strftime("%Y-%m-%d %H:%M:%S")
+    record.clock_out_time&.strftime("%Y-%m-%d %H:%M:%S")
   end
   attribute :duration
 
