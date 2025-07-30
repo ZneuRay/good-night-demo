49507477c4d9b1963ad3d818c849125fb3cf11c3
Ray <ray9015@gmail.com>
2025-07-30 17:16:31 +0800

    feat(sleep_records): implement index action to retrieve user's sleep records

--- app/controllers/api/users/sleep_records_controller.rb
+++ app/controllers/api/users/sleep_records_controller.rb
@@ -1,7 +1,10 @@
 class Api::Users::SleepRecordsController < ApplicationController
 
-  def index
+  before_action :authenticate_user!
 
+  def index
+    @sleep_records = current_user.sleep_records
+    render json: SleepRecordsSerializer.from(@sleep_records), status: :ok
   end
 
   def clock_in
